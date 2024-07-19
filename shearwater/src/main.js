fe191dcc9161f0c3f60d4c9728ebf9ed8145d4e3
builder555 <85308587+builder555@users.noreply.github.com>
2024-07-19 06:54:04 -0400

    added vue-router

--- shearwater/src/main.js
+++ shearwater/src/main.js
@@ -1,7 +1,10 @@
-// import './assets/water.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { router } from './routes';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
