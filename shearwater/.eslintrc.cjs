692a7881a454e098c983684267d8471772fbd598
builder555 <85308587+builder555@users.noreply.github.com>
2024-07-22 20:29:00 -0400

    better lint rules

--- shearwater/.eslintrc.cjs
+++ shearwater/.eslintrc.cjs
@@ -1,14 +1,24 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    // ensure semicolons
+    'semi': ['error', 'always'],
+    // ensure trailing commas
+    'comma-dangle': ['error', 'always-multiline'],
+    // ensure single quotes
+    'quotes': ['error', 'single'],
+    // ensure no unused variables
+    'no-unused-vars': 'error',
+  },
+};
