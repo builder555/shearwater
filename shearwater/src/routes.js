71f98ab2598e272c4a946d57b1573da803648f90
builder555 <85308587+builder555@users.noreply.github.com>
2024-07-28 18:46:47 -0400

    use hashes to work with incompatible servers

--- shearwater/src/routes.js
+++ shearwater/src/routes.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import HomeView from './views/HomeView.vue';
 import BluetoothDisabledView from './views/BluetoothDisabledView.vue';
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
