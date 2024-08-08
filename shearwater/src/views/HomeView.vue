d6f739b07bbad99cc204051adb43b6684afcfb1b
builder555 <85308587+builder555@users.noreply.github.com>
2024-08-07 22:03:15 -0400

    when downloading dives separately, ensure they appear in order

--- shearwater/src/views/HomeView.vue
+++ shearwater/src/views/HomeView.vue
@@ -3,11 +3,23 @@ import DiveCard from '@/components/dive-card.vue';
 import WebpImage from '@/components/webp-image.vue';
 import { useMainStore } from '@/store';
 import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 
 const store = useMainStore();
 const { diveCardClicked } = store;
 const { dives, isConnected } = storeToRefs(store);
-
+function compareDates(d1, d2) {
+  const date1 = new Date(d1);
+  const date2 = new Date(d2);
+  if (date1 > date2) {
+    return -1;
+  }
+  if (date1 < date2) {
+    return 1;
+  }
+  return 0;
+}
+const sortedDives = computed(() =>  Object.values(dives.value).sort((a, b) => compareDates(a.diveStart, b.diveStart)));
 </script>
 <template>
     <div
@@ -18,12 +30,12 @@ const { dives, isConnected } = storeToRefs(store);
     </div>
     <div class="dives">
       <span
-        v-for="(dive, id) in dives"
-        :key="id"
+        v-for="dive in sortedDives"
+        :key="dive.id"
       >
         <RouterLink
           v-if="dive.isDownloaded"
-          :to="`/dive/${id}`"
+          :to="`/dive/${dive.id}`"
         >
           <DiveCard :dive="dive" />
         </RouterLink>
