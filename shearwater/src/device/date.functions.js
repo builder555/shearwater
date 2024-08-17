491a4b4044a19fe3b2448248e88584cbafed489d
builder555 <85308587+builder555@users.noreply.github.com>
2024-08-16 22:55:14 -0400

    date functions in their own file

---
+++ shearwater/src/device/date.functions.js
@@ -0,0 +1,17 @@
+export function getHhMmSs(seconds) {
+  const hr = Math.floor(seconds / 3600);
+  const min = Math.floor((seconds % 3600) / 60);
+  const sec = seconds % 60;
+  return `${hr}h ${min}m ${sec}s`;
+}
+
+export function getDaysHhMm(minutes) {
+  const day = Math.floor(minutes / 1440);
+  const hr = Math.floor((minutes % 1440) / 60);
+  const min = minutes % 60;
+  return `${day}d ${hr}h ${min}m`;
+}
+
+export function getDate(timestamp) {
+  return new Date(timestamp * 1000).toISOString().replace('T', ' ').slice(0, 19);
+}
