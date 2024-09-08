1a01a19d7a81f7b0dbd44c3576fedd3a6e7f5691
builder555 <85308587+builder555@users.noreply.github.com>
2024-09-08 16:16:23 -0400

    fix: remove gauge from general details

--- shearwater/src/components/dive-details-general.vue
+++ shearwater/src/components/dive-details-general.vue
@@ -1,21 +1,5 @@
 <script setup>
-import { onMounted } from 'vue';
-import {RadialGauge} from 'canvas-gauges';
-const props = defineProps(['dive']);
-onMounted(() => {
-  new RadialGauge({
-      renderTo: 'canv-id',
-      value: props.dive.surface_pressure_mbars / 1000,
-      units: 'Bar',
-      title: 'Surface Pressure',
-      majorTicks: [0.8, 0.9, 1, 1.1, 1.2, 1.3],
-      minValue: 0.8,
-      maxValue: 1.3,
-      highlights: [],
-      width: 250,
-      borders: false,
-  }).draw();
-});
+defineProps(['dive']);
 </script>
 <template>
   <div class="general">
@@ -43,7 +27,6 @@ onMounted(() => {
       </div>
     </div>
   </div>
-  <canvas id="canv-id"></canvas>
 </template>
 <style lang="scss" scoped>
 .flex {
