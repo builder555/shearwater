3747b7c98be0ef2af6d8c1f3636da671061f736e
builder555 <85308587+builder555@users.noreply.github.com>
2024-09-08 16:16:42 -0400

    feat: started separate component for TI details

---
+++ shearwater/src/components/transmitter-detailes.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps(['dive']);
+</script>
+<template>
+  <div>
+    {{ dive }}
+  </div>
+</template>
+<style scoped>
+  div {
+    background-color: #fff;
+    width: 300px;
+  }
+</style>
