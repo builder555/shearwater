cfad2ca434ee902e232255da47efe72b17fb1198
builder555 <85308587+builder555@users.noreply.github.com>
2024-09-08 16:15:55 -0400

    feat: pressure gauge is its own component

---
+++ shearwater/src/components/pressure-gauge.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted } from 'vue';
+const props = defineProps(['dive']);
+onMounted(() => {
+  new RadialGauge({
+    renderTo: 'gauge',
+    value: props.dive.surface_pressure_mbars / 1000,
+    units: 'Bar',
+    title: 'Surface Pressure',
+    majorTicks: [0.8, 0.9, 1, 1.1, 1.2, 1.3],
+    minValue: 0.8,
+    maxValue: 1.3,
+    highlights: [],
+    width: 250,
+    borders: false,
+  }).draw();
+});
+
+</script>
+<template>
+  <canvas id="gauge"></canvas>
+</template>
