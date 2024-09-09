34d4cf7c125c1241b6039a6e56c376cb73129a20
builder555 <85308587+builder555@users.noreply.github.com>
2024-09-08 21:46:02 -0400

    feat: implemented transmitter details

--- shearwater/src/components/transmitter-detailes.vue
+++ shearwater/src/components/transmitter-detailes.vue
@@ -1,14 +1,30 @@
 <script setup>
-defineProps(['dive']);
+import { ref } from 'vue';
+const props = defineProps(['dive', 'transmitter']);
+const transmitter = ref({
+  name: props.dive[`ai_t${props.transmitter}_name`],
+  serial: props.dive[`ai_t${props.transmitter}_serial`],
+  max_psi: props.dive[`ai_t${props.transmitter}_max_psi`],
+  reserve_psi: props.dive[`ai_t${props.transmitter}_reserve_psi`],
+});
 </script>
 <template>
-  <div>
-    {{ dive }}
+  <div class="air-transmitter flex">
+    <div><img src="@/assets/transmitter.png"></div>
+    <div class="striped-rows" style="flex-grow:2">
+      <div>Air Transmitter: {{ transmitter.name }}</div>
+      <div>Serial: {{ transmitter.serial }}</div>
+      <div>Max PSI: {{ transmitter.max_psi }}</div>
+      <div>Reserve PSI: {{ transmitter.reserve_psi }}</div>
+    </div>
   </div>
 </template>
 <style scoped>
-  div {
+  .air-transmitter {
     background-color: #fff;
     width: 300px;
+    color: #333;
+    border-radius: 10px;
+    overflow: hidden;
   }
 </style>
